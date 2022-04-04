1c7e5e0a1a82a7291a9c44db35927f6d1a1b94b4
Cykalele <hauserthommy@web.de>
2022-04-04 22:21:47 +0200

    dd

--- app.py
+++ app.py
@@ -20,9 +20,18 @@ def send_form():
     long_url = request.form.get("long_url")
     HTTP_LOGIC_APP = "https://prod-02.northcentralus.logic.azure.com:443/workflows/472d520b360c4f8e8a0bb6f0ed0af76f/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=UQ76AMjGyzFqjZHTlIUybvYqDZMKJQnozAnDexjUXvY"
     requests.post(HTTP_LOGIC_APP, json={"long_url": long_url, "id": id})
-       
+    print("OUTGOING " + id + " --" + long_url)
+    if request.method == "POST":
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            response_json = request.get_json()
+            print( "Received HTTP Request")
+            print(response_json)
+            return response_json   
     return render_template('index.html')
 
+
+'''
 @app.route("/api/receive", methods=['POST'])
 def receive_response():  
     if request.method == "POST":
@@ -33,7 +42,7 @@ def receive_response():
             print(response_json)
             return response_json
 
-'''
+
 @app.route('/', methods=['POST'])
 def result():
     print(request.form['foo']) # should display 'bar'
