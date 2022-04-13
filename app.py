964d02aea6ad77c5d477b010f07afe8910440ae4
Cykalele <hauserthommy@web.de>
2022-04-13 23:16:22 +0200

    ad

--- app.py
+++ app.py
@@ -68,9 +68,10 @@ def redirect(shortcode):
     try:
         entry = collection.find_one({'_id': shortcode})
         print(entry)
-        longURLVal = entry['long_url']
-        print(longURLVal)
-        return redirect(longURLVal)
+        url_json = entry['long_url']
+        print(url_json)
+        longURLstring = str(url_json)
+        return redirect(longURLstring)
     except:
         return render_template('post.html', shortcode="ENTRY NOT FOUND")
 
