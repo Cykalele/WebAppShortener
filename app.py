ce549fdab6f6292749ad26447ae4ea2b6c1aac73
Cykalele <hauserthommy@web.de>
2022-04-14 16:34:25 +0200

    dd

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def redirect(shortcode):
         url_json = entry['long_url']
         print(url_json)
         #longURLstring = str(url_json)    
-        return redirect(url_json, 302)
+        return redirect(url_json)
     except Exception as ex:
         print(ex)
         return render_template('post.html', shortcode=str(ex))
