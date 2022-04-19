204cc70b45ff9503facdcaf9a571da73d6b5f21b
Cykalele <hauserthommy@web.de>
2022-04-19 15:21:01 +0200

    implemented custom api

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def home():
 def send_form():
     id = random.randint(9999, 999999)
     long_url = request.form.get("long_url")
-    HTTP_LOGIC_APP = "https://prod-02.northcentralus.logic.azure.com:443/workflows/472d520b360c4f8e8a0bb6f0ed0af76f/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=UQ76AMjGyzFqjZHTlIUybvYqDZMKJQnozAnDexjUXvY"
-    #API_URL = "https://api-appshortener.azure-api.net/LogicAppAPI/sendFormData"
-    #header = {"Ocp-Apim-Subscription-Key": "9a056297fa0f4d44ad5a6b3febf5c4fc"}
-    #sent_request = requests.post(API_URL, headers=header, json={"long_url": long_url, "id": id})
-    sent_request = requests.post(HTTP_LOGIC_APP, json={"long_url": long_url, "id": id})
+    #HTTP_LOGIC_APP = "https://prod-02.northcentralus.logic.azure.com:443/workflows/472d520b360c4f8e8a0bb6f0ed0af76f/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=UQ76AMjGyzFqjZHTlIUybvYqDZMKJQnozAnDexjUXvY"
+    API_URL = "https://apimanagementccshortener.azure-api.net/LogicApp/logicapp"
+    header = {"Ocp-Apim-Subscription-Key": "8d0c0f605b874d7dbb26f29b3a003256"}
+    sent_request = requests.post(API_URL, headers=header, json={"long_url": long_url, "id": id})
+    #sent_request = requests.post(HTTP_LOGIC_APP, json={"long_url": long_url, "id": id})
     response_body = sent_request.json()
     
     if (id == response_body['id']):
@@ -48,6 +48,7 @@ def send_form():
             return render_template('index.html', success_short_url=requested_short_url, long_url = requested_long_url)
     else:
         return render_template('index.html')  
+
 
 @app.route("/<shortcode>", methods=['GET'])
 def router(shortcode): 
